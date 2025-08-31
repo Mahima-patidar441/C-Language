44b2f3f9d09dde243ee738bea3aea54d139e8995
Mahima-patidar441 <mahimapatidar776@gmail.com>
2025-08-31 15:43:14 +0530

    Create Multiplication-Array.c

---
+++ SIG/Multiplication-Array.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[3][3], b[3][3], mul[3][3];
+    int i, j, k;
+
+    printf("Enter elements of first 3x3 matrix:\n");
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second 3x3 matrix:\n");
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            mul[i][j] = 0;
+        }
+    }
+
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            for(k = 0; k < 3; k++)
+            {
+                mul[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    printf("\nThe Multiplication of the two matrices is:\n");
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            printf("%d\t", mul[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
