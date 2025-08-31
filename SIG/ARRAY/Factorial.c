430c03b43d97d60338ef9b5a7b9c6d13c32cae87
Mahima-patidar441 <mahimapatidar776@gmail.com>
2025-08-31 15:25:40 +0530

    Create Factorial.c

---
+++ SIG/ARRAY/Factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n , fact = 1 , i = 1 ;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    
+
+    while(i<=n){
+        fact = fact * i ;
+        i++;
+    }
+    printf("the factorial of %d is %d ", n , fact);
+    return 0;
+}
