c7f69f01f72dc7f3d841c72dc2f08c078bf30bca
Mahima-patidar441 <mahimapatidar776@gmail.com>
2025-11-14 09:22:04 +0530

    Update function_overloading.cpp

--- OOPM/function_overloading.cpp
+++ OOPM/function_overloading.cpp
@@ -1,36 +1,22 @@
 #include <iostream>
 using namespace std;
 
-// Area of a square (one integer)
 int area(int side) {
     return side * side;
 }
-
-// Area of a rectangle (two integers)
 int area(int length, int width) {
     return length * width;
 }
 
-// Area of a circle (one float)
 float area(float radius) {
     return 3.14f * radius * radius;
 }
 
 int main() {
-    int s, l, w;
-    float r;
-
-    cout << "Enter side of square: ";
-    cin >> s;
-    cout << "Area of square = " << area(s) << endl;
-
-    cout << "\nEnter length and width of rectangle: ";
-    cin >> l >> w;
-    cout << "Area of rectangle = " << area(l, w) << endl;
-
-    cout << "\nEnter radius of circle: ";
-    cin >> r;
-    cout << "Area of circle = " << area(r) << endl;
+    
+    cout << "Area of square = " << area(4) << endl;
+    cout << "Area of rectangle = " << area(4,5) << endl;
+    cout << "Area of circle = " << area(2.5) << endl;
 
     return 0;
 }
